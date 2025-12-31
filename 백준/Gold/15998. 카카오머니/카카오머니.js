7d5505f1fffc88a98f1c950641772164c3c71c21
KYUNG-BOK <kyeongbok_0627@kakao.com>
2025-12-31 11:13:53 +0900

    refactor:변수명 변경, RETURN 부분 수정

--- 백준/Gold/15998. 카카오머니/카카오머니.js
+++ 백준/Gold/15998. 카카오머니/카카오머니.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const data = fs.readFileSync(0,"utf8").trim();
 if(data.length===0) {
     console.log("1");
-    return;
+    process.exit(0);
 }
 
 const input = data.split(/\s+/);
@@ -12,58 +12,58 @@ const N = Number(input[ptr++]);
 function gcd(a, b){
     while(b!==0n){
         const t=a%b;
-        a=b
-        b=t
+        a=b;
+        b=t;
     }
     return a<0n ? -a:a;
 }
 
-let cur=0n;
-let g=0n;
-let maxB=0n;
-let valid=true;
+let balance=0n;
+let m=0n;
+let maxSeenBalance=0n;
+let isValid=true;
 
 for(let i=0; i<N; i++){
     if (ptr+1 >= input.length){
-        valid=false;
+        isValid=false;
         break;
     }
     const a = BigInt(input[ptr++]);
     const b = BigInt(input[ptr++]);
     
     if(a>0n){
-        if(cur+a!==b){
-            valid = false;
+        if(balance+a!==b){
+            isValid = false;
             break;
         }
-        cur=b;
+        balance=b;
     } else {
         const x=-a;
         
-        if(cur>=x){
-            if(cur-x!==b){
-                valid=false;
+        if(balance>=x){
+            if(balance-x!==b){
+                isValid=false;
                 break;
             }
-            cur=b;
+            balance=b;
         } else {
-            const diff=b+x-cur;
+            const diff=b+x-balance;
             if(diff<=0n){
-                valid=false;
+                isValid=false;
                 break;
             }
-            g=gcd(g, diff);
-            if(b>maxB) maxB=b;
-            cur=b;
+            m=gcd(m, diff);
+            if(b>maxSeenBalance) maxSeenBalance=b;
+            balance=b;
         }
     }
 }
-if(!valid){
+if(!isValid){
     console.log("-1");
-} else if (g===0n){
+} else if (m===0n){
     console.log("1");
-} else if (g<=maxB){
+} else if (m<=maxSeenBalance){
     console.log("-1");
 } else {
-    console.log(g.toString());
+    console.log(m.toString());
 }
