6bca918d8890c52ccb5265202e5843095c162b72
KyungBok <kyeongbok_0627@kakao.com>
2026-02-18 20:18:31 +0900

    Create 조건에 맞는 사원 정보 조회하기.sql

---
+++ 프로그래머스/2/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,26 @@
+SELECT
+    g.SCORE,
+    e.EMP_NO,
+    e.EMP_NAME,
+    e.POSITION,
+    e.EMAIL
+FROM HR_EMPLOYEES e
+JOIN (
+    SELECT
+        EMP_NO,
+        SUM(SCORE) AS SCORE
+    FROM HR_GRADE
+    WHERE YEAR = 2022
+    GROUP BY EMP_NO
+) g
+ON e.EMP_NO = g.EMP_NO
+WHERE g.SCORE = (
+    SELECT MAX(SUM_SCORE)
+    FROM (
+        SELECT SUM(SCORE) AS SUM_SCORE
+        FROM HR_GRADE
+        WHERE YEAR = 2022
+        GROUP BY EMP_NO
+    ) t
+)
+ORDER BY e.EMP_NO;
