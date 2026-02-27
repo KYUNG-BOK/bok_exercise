45798f75809e6495016808e814b3e87794cdfa59
KyungBok <kyeongbok_0627@kakao.com>
2026-02-27 20:55:15 +0900

    [level 1] Title: 최소직사각형, Time: 0.74 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,10 @@
+def solution(sizes):
+    w = h = 0
+    for a, b in sizes:
+        if a < b:
+            a, b = b, a
+        if a > w:
+            w = a
+        if b > h:
+            h = b
+    return w * h
