ebc1a3fbc4eee3023c971417dfcbd16c3c3ac62b
KyungBok <kyeongbok_0627@kakao.com>
2026-02-28 17:49:15 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,8 @@
+def solution(brown, yellow):
+    total = brown + yellow
+    for h in range(3, int(total ** 0.5) + 1):
+        if total % h:
+            continue
+        w = total // h
+        if w >= h and (w - 2) * (h - 2) == yellow:
+            return [w, h]
