4abf55f731222ab07e8727a7ce9b6f776fa99c04
KyungBok <kyeongbok_0627@kakao.com>
2026-02-28 17:38:50 +0900

    [level 0] Title: 2의 영역, Time: 8.48 ms, Memory: 14.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    first = last = -1
+    for i, v in enumerate(arr):
+        if v == 2:
+            if first == -1:
+                first = i
+            last = i
+    if first == -1:
+        return [-1]
+    return arr[first:last+1]
